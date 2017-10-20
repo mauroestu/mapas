62da821e512db7be61a63dc394a1bc5ba7ec3933
mauroestu <mauroestu18@gmail.com>
2017-10-20 11:32:50 -0600

    mapas

---
+++ controllers/dream.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const information = require('../models/model');
+
+function getAllInformation(req,res){
+	information.find({},(err,data) =>{
+		if(err) return res.status(500).send({message: 'Error al realizar la peticion.'});
+		if(!data) return res.status(404).send({message: 'No hay informacion guardada.'});
+
+		res.jsonp(200,{data});
+	});
+}
+
+function saveInformation(req,res){
+	let data = new information();
+	
+	data.co2 = req.body.co2;
+	data.cx = req.body.cx;
+	data.cy = req.body.cy;
+
+	data.save((err,fact) => {
+		if(err) res.status(500).send({message: 'Error al guardar en la BD.'}); 
+		else console.log('Dato guardado.'); 		
+	});
+	res.status(200).jsonp({message: 'Dato guardado.'});
+}
+
+function obtainInformation(req,res){
+    let predictionDate = req.params.date;
+    
+    information.find({fecha:predictionDate},(err,prediccion) =>{
+        res.status(200).jsonp({prediccion});
+    });
+}
+
+function deleteInformation(req,res){
+	information.find({},(err,data) =>{
+		if(err) res.status(500).send({message: 'Error al borrar la informacion.'});
+
+		information.remove((err) => { 
+			if(err) res.status(500).send({message: 'Error al borrar la informacion.'});
+			res.status(200).send({message: 'La data ha sido eliminada.'});
+		});
+	});
+}
+
+module.exports = {
+	getAllInformation,
+	saveInformation,
+	obtainInformation,
+	deleteInformation
+}
+
---
+++ models/model.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let hoy = new Date();
+let fecha = hoy.getDate() + (hoy.getMonth() + 1) +  hoy.getFullYear();
+
+const InformationSchema = Schema({
+	cx : {type: Number, required: true},
+	cy: {type: Number, required: true},
+	co2: {type: Number, required: true},
+    fecha: { type: Number, default: fecha}
+});
+
+module.exports = mongoose.model('information',InformationSchema);
