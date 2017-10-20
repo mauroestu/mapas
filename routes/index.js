8f354231da8abbdc70f576313773cbdd20e48949
mauroestu <mauroestu18@gmail.com>
2017-10-20 16:53:11 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,7 @@ const api = express.Router();
 
 api.get('/information',InformationCtrl.getAllInformation);
 api.get('/information/:date',InformationCtrl.obtainInformation);
-<<<<<<< HEAD
 api.get('/save-information/:co2/:cx/:cy',InformationCtrl.saveInformation);
-=======
-api.get('/save-information/:value',InformationCtrl.saveInformation);
->>>>>>> origin/master
 api.delete('/delete-information',InformationCtrl.deleteInformation);
 
 module.exports = api;
